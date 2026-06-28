fb76f73f23cc7d849c2fdae97fc7c0714c87fe70
Shreya Sarjare <shreyasarjare08@gmail.com>
2026-06-28 13:18:46 +0530

    added comments test_parser updated\\ queries

--- test_parser.py
+++ test_parser.py
@@ -23,3 +23,22 @@ while True:
     question = input("Ask : ")
 
     pprint(parser.parse(question))
+
+# SELECT
+#     a.table_name,
+#     a.column_name,
+#     c_pk.table_name AS referenced_table,
+#     b.column_name AS referenced_column
+# FROM all_cons_columns a
+# JOIN all_constraints c
+#     ON a.owner = c.owner
+#    AND a.constraint_name = c.constraint_name
+# JOIN all_constraints c_pk
+#     ON c.r_owner = c_pk.owner
+#    AND c.r_constraint_name = c_pk.constraint_name
+# JOIN all_cons_columns b
+#     ON c_pk.owner = b.owner
+#    AND c_pk.constraint_name = b.constraint_name
+#    AND a.position = b.position
+# WHERE c.constraint_type = 'R'
+# AND a.owner = 'FA_USER';
